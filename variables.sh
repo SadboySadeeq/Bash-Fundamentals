1f872e7f2f4923c653bf7d70c3bad8abb002e14e
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-15 10:35:07 -0500

    Wrote down all data types for variables

--- variables.sh
+++ variables.sh
@@ -2,8 +2,8 @@
 
 #At its Simplest explanation a variable is something that holds something else
 #Variables work by storing data inside a named container
-#These data types could be 
+#These data types could be Booleans(Ture or false statements),Strings,Int,Float,Double,Char
 
 canteen_fries="that will be 4.65$"
 
-echo $canteen_fries
+echo $canteen_fries	
