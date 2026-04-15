3020dd2ce68deb936de31b1ba4968dcc9d9cfc9f
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-15 10:08:24 -0500

    Built up the explantation, had to stop to work on hack the box

--- variables.sh
+++ variables.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #At its Simplest explanation a variable is something that holds something else
+#Variables work by storing data inside a named container
+#These data types could be 
 
 canteen_fries="that will be 4.65$"
 
