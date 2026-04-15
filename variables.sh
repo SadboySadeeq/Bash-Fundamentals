9799b3498844a4716184346fd75119d0ab024377
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-15 10:43:39 -0500

    Added a explatnation for each data type

--- variables.sh
+++ variables.sh
@@ -2,7 +2,12 @@
 
 #At its Simplest explanation a variable is something that holds something else
 #Variables work by storing data inside a named container
-#These data types could be Booleans(Ture or false statements),Strings,Int,Float,Double,Char
+#These data types could be Booleans,Strings,Integers,Float,Double
+#Booleans are ture or false statements
+#Strings are texts that can include letters, numbers, symbols, or spaces
+#Integers are strings that are shown as numbers
+#Floats allow you to handle math in your scripts
+#Double allows you to handle decimal numbers in your scripts
 
 canteen_fries="that will be 4.65$"
 
