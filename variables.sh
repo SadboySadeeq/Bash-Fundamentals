7febb4a24fb337c1e5f63d6e06920464765ce939
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-16 09:54:33 -0500

    Added common mistakes found with variables

--- variables.sh
+++ variables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#How do variables work:
 #At its Simplest explanation a variable is something that holds something else
 #Variables work by storing data inside a named container
 #These data types could be Booleans,Strings,Integers,Float,Double
@@ -8,6 +9,11 @@
 #Integers are strings that are shown as numbers
 #Floats allow you to handle math in your scripts
 #Double allows you to handle decimal numbers in your scripts
+
+#Common issues or mistakes:
+#Forgetting to use $ when calling a variable
+#You can't have a space after your equals sign
+#Leaving undefined variables in your code
 
 canteen_fries="that will be 4.65$"
 
