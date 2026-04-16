e30955eff98289e3833d45ee37a0c73e1bd3dcfc
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-16 10:25:53 -0500

    Explaintion of what they are and their two types

--- quotations.sh
+++ quotations.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#What are quotations?
+#quotations are how text is understood, they are mainly used with variables
+
+#There are two types of variables:
+# " " (Double quotation) allows variables to expand and keeps spaces 
+# ' ' (Single quotation) treats everything literally
+
 cost=4.65
 
 a="that will be $cost$"
