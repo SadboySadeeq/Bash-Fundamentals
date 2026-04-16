cb95e30acc67a816ad21526a858f1692ef0dc88f
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-16 10:40:38 -0500

    Added common issues

--- quotations.sh
+++ quotations.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 #What are quotations?
+
 #quotations are how text is understood, they are mainly used with variables
 
 #There are two types of variables:
+
 # " " (Double quotation) allows variables to expand and keeps spaces 
 # ' ' (Single quotation) treats everything literally
+
+#Common mistakes/issues:
+
+#Using single quotes when you need to use a variable
+#Not quoting variables at all
+#Using double quotes when you want a literal statement
 
 cost=4.65
 
