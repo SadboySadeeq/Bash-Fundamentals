60cb25dde94d63faadb7f7e90e9d0cd62149e830
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-17 10:13:46 -0500

    Talked about how it works

--- printf.sh
+++ printf.sh
@@ -2,11 +2,17 @@
 
 #What is it?
 
-#printf formats your output way more precisely than echo
+#printf formats your output way more precisely than echo, honestly just makes your outputs look more pretty
 
 #How it works:
 
-#
+#You write your line like this: printf "format" arguments
+#The format string tells your computer how to display the values you set
+# %s is a placeholder for a string string
+# \n makes a new line
+# %d is a placeholder for a integer (numbers)
+
+
 name="Sadeeq"
 age="16"
 
