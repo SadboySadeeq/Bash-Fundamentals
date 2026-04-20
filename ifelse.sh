7c838f1e74cd2f40ab6bd085137ae8c62f73c1a8
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-20 10:13:07 -0500

    talked about how it works,might expand later

--- ifelse.sh
+++ ifelse.sh
@@ -4,5 +4,11 @@
 
 #They are used to make decisions in your script, allowing your computer to choose what to do depending on what the conditions are
 
+#How it works:
+
+#if > first condition
+#elif > is when you have more than one condition
+#else > fallback, works like a elif but you can't chain another condition after it's been called
+#fi > used to end the block of code you wrote
 #
 
