f9bb4f51b35d2ee7e07899dab7ae97e0b7f8f9ee
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-20 10:07:49 -0500

    explained what ifelse is

--- ifelse.sh
+++ ifelse.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+#What are if / elif / else?
+
+#They are used to make decisions in your script, allowing your computer to choose what to do depending on what the conditions are
+
+#
+
