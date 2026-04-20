9630d9bae36a443262340b53bb3238c6f9d8d640
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-20 10:27:09 -0500

    Added common mistakes

--- ifelse.sh
+++ ifelse.sh
@@ -10,8 +10,15 @@
 #elif > is when you have more than one condition
 #else > fallback, works like a elif but you can't chain another condition after it's been called
 #fi > used to end the block of code you wrote
-#
+#From your computers pov this is how it handles it: Check if > If true then run that block and skip the rest > If false then check elif > If none are true > run else
 
+#Common mistakes
+
+#Forgetting spaces, ex. if[$grade -ge 90]; then
+#Not quoting variables, ex. if [ $name = Sadeeq ]; then
+#using = for numbers, ex, if [ "$grade" = 90 ]; then
+#Forgetting to finish your line with then, ex. if [ "$grade" -ge 90 ]
+#Forgetting to end code with fi, ex. if [ "$grade" -ge 90 ]; then echo "W grade"
 
 
 
