374bcd4f8dc3207f6b32840dc5464fb51d820a0d
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-22 10:26:16 -0500

    Added common mistakes and taught a fancy way of using read

--- user_input.sh
+++ user_input.sh
@@ -11,6 +11,15 @@
 #The script pauses and waits for your input
 #When the user types something down and presses enter the value gets stored inside the name variable
 #You are then able to do manipulate that variable how ever you see fit, Ex. Echo "Hello $name"
+#You could be fancy and use read -P which allows you to prompt a text and collects their input in the same line, Ex. read -p "Enter your name: " name
 
 #Common mistakes:
+
+#Not quoting input, echo Hello $name, because if the user were to enter Sadeeq Lawal it gets split up
+#If the variable already has a value the read will replace whatever that old value was
+#Forgetting to prompt the user for their input either by forgetting to echo a prompt or not using -p, ex. read name
+
+#How to troubleshoot the mistakes:
+
+#
 
