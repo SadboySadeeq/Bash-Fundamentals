10824c4687e564f7d043e85315710ea693d7e95d
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-22 10:19:38 -0500

    explained what it is and how it works

--- user_input.sh
+++ user_input.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+#What are user inputs?
+
+#A user input is how your script gets info from the user instead of using fixed values
+
+#How it works:
+
+#When you have a read line, Ex. read name
+#The read is what starts the user input while the name is just the variable name
+#The script pauses and waits for your input
+#When the user types something down and presses enter the value gets stored inside the name variable
+#You are then able to do manipulate that variable how ever you see fit, Ex. Echo "Hello $name"
+
+#Common mistakes:
+
