028145a7a32c3e086b37947d09b1bd067f2772da
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-30 10:11:16 -0500

    Explained what they are and how they work

--- arrays.sh
+++ arrays.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+#What are arrays?
+
+#An array is a variable that can store multiple values instead of making a bunch of separate variables
+
+#How it works:
+
+#Arrays are made using parenthese (), Ex. Grades=("A","B","C","D","F")
+#Each value in a array has an idex(position) starting at 0
+#To acess the values use ${array[index]}
+#To acess all values use ${array[@]}, to acess a specific value replace the @ with the index of the value
+#
