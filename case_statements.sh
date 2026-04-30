ab6070b5c7769c6438425ee92f6763a9ec82b7cf
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-04-30 09:56:57 -0500

    Explained what it is and how it works

--- case_statements.sh
+++ case_statements.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+#What are case statements?
+
+#A case statement is another way to do conditonals, it's used when you want to say: "If this variable matches one of these option, do something" it's cleaner than writing a bunch of elif statements
+
+#How it works:
+
+#Bash looks at the variable, Ex.grade
+#Compares it to each pattern, Ex. A,B,C
+#Runs the first match
+#Stops after ;;
+#If nothing matches then it runs * which is default
+
+#
