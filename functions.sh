cdfac52af25074d0a451e12e49740a1f8f1819ff
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-04 10:12:16 -0500

    Made example code, going to have to redo the how it works section

--- functions.sh
+++ functions.sh
@@ -6,10 +6,18 @@
 
 #How it works:
 
+#You define the function 
+
 #function_name() {
   # you right your code here:
 #}
 
 #To run that code you call the function you just right the function name, Ex. function_name
 
-#
+
+print_abc()
+{
+	echo "A,B,C"
+}
+
+print_abc
