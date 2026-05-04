96fb15aa34d28d01c141173e7639c460df471720
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-04 10:17:35 -0500

    Troubleshoot advice

--- functions.sh
+++ functions.sh
@@ -16,6 +16,12 @@
 #Calling the function before defining it
 #Missing brackets
 
+#How to troubleshoot the mistakes:
+
+#Make sure to call the function when ever you need it
+#Make sure that you define the function first, then call it
+#Make sure to add () brackets next to the function name to actually make it a function
+
 print_abc()
 {
 	echo "A,B,C"
