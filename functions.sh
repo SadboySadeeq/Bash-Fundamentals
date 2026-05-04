60a3ce9c0abda37d4ac19ceb7d0658893afbcdd4
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-04 10:02:43 -0500

    Explained what they are and how they work

---
+++ functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#What are functions?
+
+#Functions are a named block of code that runs whenever you call it
+
+#How it works:
+
+#function_name() {
+  # you right your code here:
+#}
+
+#To run that code you call the function you just right the function name, Ex. function_name
+
+#
