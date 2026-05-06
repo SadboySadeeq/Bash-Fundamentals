22588deb5756b65a536ccf2f062880e5cacf46dc
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-06 10:02:04 -0500

    explained what loops,while and do while are.Explained how while loops work

---
+++ while_loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#What are do while/while loops?
+
+#Loops allow your script to repeat code multiple times instead of you writing the same thing over and over again
+#A while loop runs as long as a condition is true.
+#A do while run the code at least once, then check the condition
+
+#How it works (While loop):
+
+#Check the condition
+#If true > run the code
+#Go back and check again
+#Stops when condition becomes false
+
+#How it works (Do while):
