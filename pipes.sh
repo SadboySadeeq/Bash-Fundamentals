686879c94c71a6151851b51dcb5e9d1c671c61ca
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-08 10:23:17 -0500

    Indepth explanation of pipes

---
+++ pipes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#What are pipes?
+
+#Pipes are used to send the output of one command directly into another command
+#The pipe symbol is |
+#It means: Take the output from the left command and give it to the right command
+
+#How it works:
+
+#
