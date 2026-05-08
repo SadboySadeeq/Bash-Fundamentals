23ec81ace8b6bb4c6c55211709224602ab89ac00
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-08 10:11:37 -0500

    Explained what 2d arrays are and how they work

---
+++ 2d_arrays.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#What are Two dimensional arrays?
+
+#A two dimensional array is an array inside another array, like a table with rows and columns
+
+#How it works:
+
+#Main array stores rows
+#You turn one row into another array
+#Access column
+
+#
