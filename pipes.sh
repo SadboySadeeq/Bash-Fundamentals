16adb839893b7ead9137e35b76b03b0bc17ac614
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-08 10:26:44 -0500

    Explained how it works with a really simple script

--- pipes.sh
+++ pipes.sh
@@ -3,9 +3,16 @@
 #What are pipes?
 
 #Pipes are used to send the output of one command directly into another command
-#The pipe symbol is |
+#The pipe symbol is (|)
 #It means: Take the output from the left command and give it to the right command
 
+#Sample code for better explain how it works:
+#echo "Hello" | cat
+
 #How it works:
+
+#echo "Hello" outputs: Hello
+#Pipe (|) sends it to cat
+#cat prints it
 
 #
