87e84cdb181a55edec64c1ed8419694e40c0a1b6
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-11 10:02:44 -0500

    Went in depth as to what for loops are

---
+++ for_loops.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#What are for loops?
+
+#A for loop is used to repeat code a certain number of times or for each item in a list.
+#Instead of writing the same code over and over, a for loop automates it.
+#A for loop means: For each item, run this code
+#
