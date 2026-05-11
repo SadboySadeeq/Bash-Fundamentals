c9814636c5b5a98cd2da3507af422e4712a1d4c4
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-11 09:55:17 -0500

    Explained what they are,how it works,sample code to better explain how it works

---
+++ redirect.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#What are redirects?
+
+#Redirects are used to send inputs or outputs somewhere else instead of the terminal
+#Normally:
+#commands print output to the screen
+#commands receive input from the keyboard
+#Redirects let you change that
+
+#How it works:
+
+#Example code to better explain: echo "Hello" > file.txt
+
+#echo "Hello" creates output
+#> redirects it into file.txt
+#File contents become: Hello
+#> overwrites the file, if the file.txt already had something inside it gets replaced
+
+#Common mistakes:
+
+#
