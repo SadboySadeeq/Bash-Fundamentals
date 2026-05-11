669ecce6f2836e1f1a99eb325b965d5698200837
slawal <slawal@Sandbox.myguest.virtualbox.org>
2026-05-11 10:03:33 -0500

    Explained how it works

--- for_loops.sh
+++ for_loops.sh
@@ -5,4 +5,13 @@
 #A for loop is used to repeat code a certain number of times or for each item in a list.
 #Instead of writing the same code over and over, a for loop automates it.
 #A for loop means: For each item, run this code
+
+#How it works:
+
+#Bash takes the first item
+#Stores it in the variable
+#Runs the code
+#Moves to the next item
+#Repeats until finished
+
 #
